a9914dd89669d25be0bee0ab10b4a1d281ff7eed
Mitja Pak <pmitja1@gmail.com>
2023-08-21 23:47:46 +0200

    Removed console log.

--- components/CompaniesBanner.tsx
+++ components/CompaniesBanner.tsx
@@ -2,7 +2,6 @@ import { storyblokEditable } from '@storyblok/react/rsc';
 import Image from 'next/image';
 
 const CompaniesBanner = ({ blok }: any) => {
-  console.log(blok);
   return (
     <div
       className='flex h-[120] flex-row flex-wrap justify-between gap-[8px] bg-gray-300 p-7'
