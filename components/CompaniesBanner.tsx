4695a3ce29f7afa314e8fe61a6f596918ae1006a
Mitja Pak <pmitja1@gmail.com>
2023-08-22 22:18:21 +0200

    Fixed the issue.

--- components/CompaniesBanner.tsx
+++ components/CompaniesBanner.tsx
@@ -2,12 +2,13 @@ import { CompaniesBannerStoryblok } from '@/component-types-sb';
 import { storyblokEditable } from '@storyblok/react/rsc';
 import Image from 'next/image';
 
-const CompaniesBanner = (blok: CompaniesBannerStoryblok) => {
+const CompaniesBanner = ({ blok }: { blok: CompaniesBannerStoryblok }) => {
   return (
     <div
       className='flex h-[120] flex-row flex-wrap justify-between gap-[8px] bg-gray-300 p-7'
       {...storyblokEditable(blok)}
     >
+      {blok.component}
       {blok.companiesLogo.map((company) => (
         <Image
           key={company.id}
