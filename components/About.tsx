ea90330a0636d6402a157065175dfc7473d88f73
Mitja Pak <pmitja1@gmail.com>
2023-08-22 23:38:02 +0200

    Fixed the font.

--- components/About.tsx
+++ components/About.tsx
@@ -5,7 +5,7 @@ const About = ({ blok }: { blok: AboutStoryblok }) => {
   console.log(blok);
   return (
     <div className='md:px-18 flex flex-row gap-8 px-12 py-32 text-left lg:px-24'>
-      <div className='flex flex-col gap-4 sm:gap-5 md:gap-6 lg:gap-8'>
+      <div className='flex flex-col place-content-center gap-4 sm:gap-5 md:gap-6 lg:gap-8'>
         <div className='relative flex items-center'>
           <div className='w-10 border-t border-gray-400'></div>
           <span className='mx-2 flex-shrink text-lg'>About us</span>
@@ -18,8 +18,11 @@ const About = ({ blok }: { blok: AboutStoryblok }) => {
         </p>
       </div>
       <div className='relative w-full justify-center'>
-        <span className='absolute left-[-7%] top-[-5%] flex flex-col rounded-xl bg-white text-5xl font-bold text-red-600'>
-          {blok.years}+<span className='text-3xl text-black'>Years</span>
+        <span className='absolute left-[-7%] top-[-5%] flex flex-col rounded-xl bg-white text-2xl font-bold text-red-600 sm:text-3xl md:text-4xl lg:text-5xl'>
+          {blok.years}+
+          <span className=' text-lg font-bold text-black sm:text-xl md:text-2xl lg:text-3xl'>
+            Years
+          </span>
         </span>
         <Image
           src={blok.image.filename}
