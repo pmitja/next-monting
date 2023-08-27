0702063c131f1cf1936a8068f8627877838a5046
Mitja Pak <pmitja1@gmail.com>
2023-08-27 13:04:22 +0200

    Added types.

--- components/CtaSection.tsx
+++ components/CtaSection.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import { Button } from './ui/button';
 import Image from 'next/image';
+import { storyblokEditable } from '@storyblok/react';
+import { CtaSectionStoryblok } from '@/component-types-sb';
 
-const CtaSection = ({ blok }) => {
+const CtaSection = ({ blok }: { blok: CtaSectionStoryblok }) => {
   return (
-    <div className='relative inset-0 my-10 flex h-[300px] flex-col justify-center bg-gradient-to-r from-red-600 to-transparent lg:mb-16'>
+    <div
+      {...storyblokEditable(blok)}
+      className='relative inset-0 my-10 flex h-[300px] flex-col justify-center bg-gradient-to-r from-red-600 to-transparent lg:mb-16'
+    >
       <Image
         src={blok.image.filename}
         alt={blok.image.alt ?? 'Cta image'}
