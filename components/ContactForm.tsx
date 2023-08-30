2898584b41ec26ad939ed145babcaf57b031588e
Mitja Pak <pmitja1@gmail.com>
2023-08-30 22:14:40 +0200

    Removed alert.

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -50,17 +50,13 @@ const ContactForm = ({ formElements }: { formElements: ContactStoryblok }) => {
   });
 
   const sendEmail = async (data: z.infer<typeof FormSchema>) => {
-    const response = await fetch('/api/send', {
+    await fetch('/api/send', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify(data),
     });
-
-    if (response.status === 200) {
-      alert('Email sent!');
-    }
   };
 
   const onSubmit = (data: z.infer<typeof FormSchema>) => {
