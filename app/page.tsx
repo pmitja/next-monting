6e49b3efcffbd5c78369b43fa5cbae7b9b432efa
Mitja Pak <pmitja1@gmail.com>
2023-08-30 23:11:15 +0200

    Added fetchConfig and fetch story for config.

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
 import StoryblokStory from '@storyblok/react/story';
 import { fetchData } from '@/utils/fetchData';
+import { fetchConfig } from '@/utils/fetchConfig';
 
 export const metadata: Metadata = {
   title: 'MONTING Plus',
@@ -11,15 +12,16 @@ export const metadata: Metadata = {
 
 export default async function Home() {
   const { data } = await fetchData();
+  const { data: config } = await fetchConfig();
 
   return (
     <>
-      <Navbar />
+      <Navbar data={config.story} _uid={''} component={'config'} />
       <main>
         <h1 className='absolute hidden'>Monting Plus d.o.o</h1>
         <StoryblokStory story={data.story} />
       </main>
-      <Footer />
+      <Footer data={config.story} _uid={''} component={'config'} />
     </>
   );
 }
